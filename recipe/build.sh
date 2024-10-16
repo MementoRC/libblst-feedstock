b3dc6053b66815ead0103916f6bf4184445add79
memento <claude.rc@gmail.com>
2024-10-16 18:25:40 -0500

    (fix) use lib instead?

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,10 +18,12 @@ if [[ "${target_platform}" == win-* ]]; then
   mkdir -p "${PREFIX}"/Library/bin
   install -m755 blst.dll "${PREFIX}"/Library/bin/blst.dll
 
-  dlltool \
-    -d build/win64/blst.def\
-    -l "${PREFIX}"/Library/lib/blst.lib \
-    -D "${PREFIX}"/Library/bin/blst.dll
+  lib_cmd="lib /def:build/win64/blst.def /out:\"${PREFIX}/Library/lib/blst.lib\" /machine:x64"
+  cmd.exe /c "$lib_cmd"
+  # dlltool \
+  #   -d build/win64/blst.def\
+  #   -l "${PREFIX}"/Library/lib/blst.lib \
+  #   -D "${PREFIX}"/Library/bin/blst.dll
 
 elif [[ "${target_platform}" == osx-* ]]; then
   mkdir -p "${PREFIX}"/lib
