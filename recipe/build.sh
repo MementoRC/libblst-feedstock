20452d7953b8f7dffc25a532a0f5713b95474939
memento <claude.rc@gmail.com>
2024-10-17 12:26:40 -0500

    (fix) win paths annoyance

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,7 @@ if [[ "${target_platform}" == win-* ]]; then
   mkdir -p "${PREFIX}"/Library/bin
   install -m755 blst.dll "${PREFIX}"/Library/bin/blst.dll
 
-  lib_cmd="lib /def:build\\win64\\blst.def /out:\"$(echo ${PREFIX} | sed 's/\//\\/g')\\\Library\\lib\\blst.lib\" /machine:x64"
+  lib_cmd="lib /def:build\\win64\\blst.def /out:\"$(echo ${PREFIX} | sed 's/\//\\/g')\\Library\\lib\\blst.lib\" /machine:x64 /verbose"
   cmd.exe /c "$lib_cmd"
   # dlltool \
   #   -d build/win64/blst.def\
