7402c21da3b84260bbe92ea691042d459556ad07
memento <claude.rc@gmail.com>
2024-11-18 15:52:09 -0600

    (fix) back to simple arg/cflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ elif [[ "${target_platform}" == osx-64 ]]; then
   B_ARGS=('-shared')
 elif [[ "${target_platform}" == osx-arm64 ]]; then
   # export CFLAGS="${CFLAGS}"
-  B_ARGS=('-shared' 'AR=arm64-apple-darwin20.0.0-llvm-ar' 'RANLIB=arm64-apple-darwin20.0.0-llvm-ranlib')
+  B_ARGS=('-shared')
 else
   B_ARGS=('-shared')
 fi
